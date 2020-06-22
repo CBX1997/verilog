137eac16f9b59a596f81c70fc23652b30bc82efa
IC-Boxuan <1159834330@qq.com>
2020-06-22 23:59:48 +0800

    add a adder 4bit code

---
+++ sources_1/new/adder4.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/06/22 23:44:21
+// Design Name: 
+// Module Name: adder4
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module adder4(cout,sum,ina,inb,cin);
+output[3:0] sum;
+output cout;
+input[3:0] ina,inb;
+input cin;
+assign {cout,sum}=ina+inb+cin;
+endmodule
