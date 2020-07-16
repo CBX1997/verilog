96fb84c9015cb0ed0c0a93fa2051fd10f068abe1
IC-Boxuan <1159834330@qq.com>
2020-07-16 23:51:44 +0800

    add a 8-bit synchronous setting asynchronous clearing counter

---
+++ sources_1/new/count8.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: XAUT_IC
+// Engineer: CBX1997
+// 
+// Create Date: 2020/07/16 23:42:27
+// Design Name: 
+// Module Name: count8
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 8-bit synchronous setting asynchronous clearing counter
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module count8(
+							clk,
+							rst,
+							out,
+							data,
+							load
+              );
+              
+input [7:0]  data;
+input load,clk,rst;
+
+output [7:0] out;
+
+
+reg[7:0] out;
+always @(posedge clk)
+      begin
+      	if(!rst) out = 8'00;
+      	else if(load) out = data;
+      	else out=out+1;
+      end
+endmodule
