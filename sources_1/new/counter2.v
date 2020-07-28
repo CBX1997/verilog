14b5866f38e2c197ea4dcb2cc3e1f868ece70e02
IC-Boxuan <1159834330@qq.com>
2020-07-28 18:18:59 +0800

    Asynchronous clearing counter described by always procedure statement

---
+++ sources_1/new/counter2.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: XAUT
+// Engineer: CBX1997
+// 
+// Create Date: 2020/07/28 10:03:22
+// Design Name: 
+// Module Name: counter2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 	Asynchronous clearing counter described 
+//                by always procedure statement
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module counter2(
+			rst,
+			clk,
+			out
+								);
+output[7:0] out;
+input clk,rst;
+reg[7:0] out;
+always @(posedge clk or negedge rst)
+		begin
+			if(!rst)
+				out=0;
+			else out=out+1;
+		end
+endmodule
