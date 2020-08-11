9a45fddf1db7dfa9a873623c9594729256ac7af1
IC-Boxuan <1159834330@qq.com>
2020-08-11 23:55:02 +0800

    no message

---
+++ sources_1/new/decoder2_4.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/08/11 23:54:03
+// Design Name: 
+// Module Name: decoder2_4
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module decoder2_4(
+
+    );
+endmodule
