f68d2988402fb31c7dea31e889a81df94c4f4b51
IC-Boxuan <1159834330@qq.com>
2020-09-09 18:39:44 +0800

    Use defparam statement to modify parameter value

---
+++ sources_1/new/re_defparam.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/09/09 16:38:31
+// Design Name: 
+// Module Name: re_defparam
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module re_defparam(
+
+    );
+endmodule
