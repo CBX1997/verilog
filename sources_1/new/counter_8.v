8158296580911219b6660dd92e66c7ec2a22315b
IC-Boxuan <1159834330@qq.com>
2020-09-11 22:24:26 +0800

    8-bit adder

---
+++ sources_1/new/counter_8.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/09/11 14:41:19
+// Design Name: 
+// Module Name: counter_8
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module counter_8(
+
+    );
+endmodule
