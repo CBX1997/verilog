bc5948c391149c81aad0dde34537d78a18f11d7a
IC-Boxuan <1159834330@qq.com>
2020-09-15 19:27:23 +0800

    decoder 2 to 4

--- sources_1/new/decoder2_4.v
+++ sources_1/new/decoder2_4.v
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: XAUT_IC
+// Engineer: CBX1997
 // 
-// Create Date: 2020/08/11 23:54:03
+// Create Date: 2020/09/10 16:02:54
 // Design Name: 
 // Module Name: decoder2_4
 // Project Name: 
@@ -21,6 +21,15 @@
 
 
 module decoder2_4(
-
-    );
+	in0,in1,en,out0,out1,out2,out3);
+	output out0,out1,out2,out3;
+	input in0,in1,en;
+		wire 	wire1,wire2;
+		not 	U1(wire1,in0),
+				U2(wire2,in1);
+		nand    U3(out0,en,wire1,wire2),
+				U4(out1,en,wire1,in1),
+				U5(out2,en,in0,wire2),
+				U6(out3,en,in0,in1);
 endmodule
+
