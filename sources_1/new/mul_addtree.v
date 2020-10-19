dfa047681e8f40d37f7f7f3e55499711d3a6beae
IC-Boxuan <1159834330@qq.com>
2020-10-19 10:36:03 +0800

    Four bit adder tree multiplier

---
+++ sources_1/new/mul_addtree.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/10/15 17:15:24
+// Design Name: 
+// Module Name: mul_addtree
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mul_addtree(
+
+    );
+endmodule
